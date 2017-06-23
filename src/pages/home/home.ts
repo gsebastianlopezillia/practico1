ac0d246a192e7a556e75f4ad8db9547d35a266ed
gsebastianlopezillia <gsebastianlopezillia@gmail.com>
2017-06-23 09:19:42 -0300

    Estado: cordova-plugin-camera-preview funcionando, platform android added

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,58 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, Platform } from 'ionic-angular';
+import { CameraPreview, CameraPreviewPictureOptions, CameraPreviewOptions, CameraPreviewDimensions } from '@ionic-native/camera-preview';
+
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  private picture : String;
 
-  constructor(public navCtrl: NavController) {
+  // picture options
+  private pictureOpts: CameraPreviewPictureOptions = {
+    width: 1000,
+    height: 1000,
+    quality: 100
+  };
+  
+    // camera options (Size and location). In the following example, the preview uses the rear camera and display the preview in the back of the webview
+  private cameraPreviewOpts: CameraPreviewOptions = {
+    x: 0,
+    y: 0,
+    width: window.screen.width,
+    height: window.screen.height,
+    camera: 'front',
+    tapPhoto: false,
+    previewDrag: false,
+    toBack: true,
+    alpha: 1
+  };
+  constructor(public navCtrl: NavController,
+              private cameraPreview: CameraPreview,
+              public platform: Platform) {
+      // start camera
+      this.cameraPreview.startCamera(this.cameraPreviewOpts).then(
+          (res) => {
+            console.log('Success preview: '+res)
+          },
+          (err) => {
+            console.log('Fail preview: '+err)
 
+      });
   }
+
+  /*CAMERA-PREVIEW*/
+  getPicture(){
+    this.cameraPreview.takePicture(this.pictureOpts).then((imageData) => {
+          this.picture = 'data:image/jpeg;base64,' + imageData;
+          console.log('Success take: '+this.picture);
+        }, (err) => {
+          console.log('Fail take: '+err);
+          this.picture = 'assets/img/test.jpg';
+        });
+  }
+  /*FIN CAMERA-PREVIEW*/
 
 }
