39667953cc2dcd2abe26dab725808965349d66ce
gsebastianlopezillia <gsebastianlopezillia@gmail.com>
2017-06-27 09:27:00 -0300

    solucionado pantallazo blanco al iniciar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -27,10 +27,11 @@ export class MyApp {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       statusBar.hide();
-      splashScreen.hide();
-      this.crearBase();
       fullScreen.immersiveMode();
-
+      setTimeout(()=>{
+        splashScreen.hide();
+      }, 100);
+      this.crearBase();
     });
   }
 
